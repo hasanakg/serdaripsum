158b88b1d41e05cc594847c55a68a47b1620e29e
Hasan Akgün <hasanakg@outlook.com>
2015-04-08 15:16:04 +0300

    Paragraf halinde yazabilmesi için birkaç ekleme
    Python bilmiyorum ama bir şeyler eklemeye çalıştım. Argüman olarak P
    alınca şiir, p alınca da paragraf şeklinde yazdırabiliyor. Duruma göre
    verilen sayı da kıta sayısı veya kelime sayısı olabiliyor.

--- serdaripsum.py
+++ serdaripsum.py
@@ -9,15 +9,23 @@ if __name__ != '__main__':
     print("Not a module!")
     sys.exit(1)
 
-parser = argparse.ArgumentParser(description='To tell me how many verses')
+parser = argparse.ArgumentParser(description='To tell me how many verses or words')
+parser = argparse.ArgumentParser(description='To tell me if it is a poem or paragraph')
+
 parser.add_argument('length', type=int, help='Verses count to generate')
+parser.add_argument('type', type=str, help='Type')
 args = parser.parse_args()
 
 PLENGTH = args.length
+PTYPE = args.type
+
 song_file = "songs.txt"
 MIN_WORDS_PER_LINE = 3
 MAX_WORDS_PER_LINE = 6
 words = []
+
+global word_count
+word_count = 0
 
 with open(song_file) as f:
     lines = f.readlines()
@@ -30,14 +38,15 @@ for line in lines:
             word.replace("\n", "")
             words.append(word.lower())
 
-
 def generate_a_line():
     random_line_length = random.randint(MIN_WORDS_PER_LINE, MAX_WORDS_PER_LINE)
     song_line = ""
     for i in range(0, random_line_length):
         random_word = random.randint(0, len(words) - 1)
         song_line += words[random_word] + " "
-    return song_line[:1].upper() + song_line[1:]
+    global word_count
+    word_count += random_line_length
+    return song_line[:1].upper() + song_line[1:len(song_line) - 1]
 
 
 def generate_verse():
@@ -45,11 +54,23 @@ def generate_verse():
         print(generate_a_line())
     print(" ")
 
+def generate_paragraph():
+    song_paragrapgh = ""
+    for i in range(0,PLENGTH):
+        song_paragrapgh += generate_a_line() + '. '
+        global word_count
+        i += word_count
+        if i >= PLENGTH:
+            break
+    return song_paragrapgh
+
 
 def generate_song():
-    for i in range(0, PLENGTH):
-        generate_verse()
-
+    if PTYPE == "P":
+        for i in range(0, PLENGTH):
+            generate_verse()
+    elif PTYPE == "p":
+        print(generate_paragraph())
 
 def song_title():
     title = words[random.randint(0, len(words) - 1)] + " " + words[random.randint(0, len(words) - 1)] + " " + words[random.randint(0, len(words) - 1)]
